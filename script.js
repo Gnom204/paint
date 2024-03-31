10eafe02b9165966a3ed2234ace680ba58111a67
Gnom204 <113632977+Gnom204@users.noreply.github.com>
2024-03-31 19:50:12 +0300

    featured: доделал

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const resetBtn = document.querySelector(".reset");
 const timer = document.querySelector(".timer");
 const second = document.querySelector(".second");
-const milSecond = document.querySelector(".tmilSecond");
+const milSecond = document.querySelector(".milsecond");
 const ctx = canvas.getContext("2d");
 
 let times = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000];
@@ -29,15 +29,57 @@ canvas.addEventListener(
   false
 );
 
+function isInteger(num) {
+  return (num ^ 0) === num;
+}
+
+function calc(sec, msec) {
+  milSecond.textContent = msec.toString();
+  second.textContent = sec;
+  while (sec === 0) {
+    if (msec <= 0) {
+      sec--;
+      msec = 99;
+      milSecond.textContent = msec;
+      second.textContent = sec;
+    } else {
+      msec--;
+      milSecond.textContent = msec;
+      second.textContent = sec;
+    }
+    clearInterval();
+  }
+}
+
 function startDrawing(e) {
   drawing = true;
+
   let time = randomTime(times);
   if (!isDraw) {
+    countdownTimer(time);
     setTimeout(() => {
       lastTime = false;
     }, time);
   }
   draw(e);
+}
+
+function countdownTimer(milliseconds) {
+  let endTime = Date.now() + milliseconds;
+
+  let timerInterval = setInterval(() => {
+    let remainingTime = endTime - Date.now();
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      second.textContent = "00";
+      milSecond.textContent = "00";
+    } else {
+      let seconds = Math.floor(remainingTime / 1000);
+      let milliseconds = remainingTime % 1000;
+      second.textContent = seconds;
+      milSecond.textContent = milliseconds;
+    }
+  }, 1); // Проверяем каждую миллисекунду
 }
 
 function draw(e) {
